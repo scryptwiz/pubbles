aecba180880de7a3b66fc5eb7e5113065075e6ab
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-09-18 19:19:02 +0100

    latest update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Footer = () => {
     const allcontent = useSelector(state=>state.mainStore)
   return (
-    <div className="main_width relative">
+    <div className="main_width relative mt-60 md:mt-0">
         <div className="max_width flex flex-col z-20 mt-10 lg:p-10 p-5 relative gap-3">
             <div className="flex justify-between md:items-center w-full flex-col-reverse md:flex-row gap-2">
                 <h1 className="title text-xl md:text-3xl text-pubblesBlue">Join Our Community</h1>
@@ -15,7 +15,7 @@ const Footer = () => {
                     <ul className="flex gap-5 py-2 w-full">
                             {allcontent.socials[0].map((item,index)=>{
                                 return(
-                                    <li>
+                                    <li key={index}>
                                         <a href={item.fields.link} target="_blank" rel="noreferrer">
                                             <img src={item.fields.image.fields.file.url} alt={item.fields.image.fields.title} className='w-5 h-5' />
                                         </a>
@@ -24,7 +24,7 @@ const Footer = () => {
                             })}
                     </ul>
                 )}
-                <ul className="flex gap-3 flex-col md:flex-row md:items-center md:w-fit w-full text-gray-600 text-sm mt-3 md:mt-0">
+                <ul className="flex gap-3 flex-col md:flex-row md:items-center md:w-full w-full text-gray-600 text-sm mt-3 md:mt-0">
                     <li><a href='#about'>About</a></li>
                     <li><a href='#roadmap'>Roadmap</a></li>
                     <li><a href='#community'>Community</a></li>
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -5,14 +5,16 @@ const NewsLetter = () => {
   return (
     <div className="main_width relative">
         {allContent.newsLetter&&(
-            <div className="max_width hidden  md:flex z-20 mt-10 lg:p-10 p-5 relative">
-                <div className=" w-full h-fit my-auto  relative">
-                    <img src={allContent.newsLetter[0][0].fields.image.fields.file.url} className='z-20 lg:w-80 w-72' alt={allContent.newsLetter[0][0].fields.image.fields.title} />
-                    <div className="bg-pubblesYellow blueShadow rounded w-[90%] absolute top-10 right-0 zindex flex justify-between">
-                        <div className="lg:w-8/12 w-8/12 py-5 px-10 ml-auto">
+            <div className="max_width mt-10 lg:p-10 p-5 relative">
+                <div className=" w-full sm:w-1/2 md:w-full mx-auto md:h-fit h-full my-auto  relative">
+                    <img src={allContent.newsLetter[0][0].fields.image.fields.file.url} className='z-20 lg:w-80 md:w-72 w-full' alt={allContent.newsLetter[0][0].fields.image.fields.title} />
+                    <div className="bg-pubblesYellow blueShadow rounded w-full md:w-[90%] absolute top-1/2 md:top-10 right-0 zindex flex justify-between">
+                        <div className="lg:w-8/12 md:w-8/12 w-full py-5 px-5 md:px-10 ml-auto md:mt-0 mt-32">
                             <h1 className="title lg:text-3xl text-2xl text-pubblesBlue">{allContent.newsLetter[0][0].fields.title}</h1>
                             <h1 className="dm_font text-pubblesBlue/80 mt-2">{allContent.newsLetter[0][0].fields.content}</h1>
-                            <div className="bg-white rounded-full w-full flex justify-between mt-2">
+                            <input type="text" placeholder="Email Here" className="text-pubblesBlue px-4 zindexes  rounded-full w-full py-2  z-40 mt-3 bg-white flex md:hidden" />
+                            <button className="btn_shadow_yellow px-5 py-2 rounded-full bg-pubblesYellow text-white mx-auto mt-3 z-40  flex md:hidden">Subscribe</button>
+                            <div className="bg-white rounded-full w-full justify-between mt-2 hidden md:flex">
                                     <input type="text" placeholder="Email Here" className="text-pubblesBlue px-4 z-40 zindexes  rounded-full w-full py-2" />
                                     <button className="btn_shadow_yellow px-5 py-2 rounded-full bg-pubblesYellow text-white">Subscribe</button>
                             </div>
